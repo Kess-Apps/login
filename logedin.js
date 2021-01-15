1030021ef42921c04499f43d59129c1d856c777a
Kess-Apps <72557998+Kess-Apps@users.noreply.github.com>
2021-01-15 17:48:49 +0100

    Add files via upload

---
+++ logedin.js
@@ -0,0 +1,10 @@
+function logout(){
+	document.cookie = "login=false";
+	window.open("index.html","_self");
+}
+function deleteAccount(){
+	document.cookie = "username=#";
+	document.cookie = "password=#";
+	document.cookie = "login=false";
+	window.open("index.html","_self");
+}
