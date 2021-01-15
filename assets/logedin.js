26230261f2bfaeb153453e247d79eebbe96962ee
Kess-Apps <72557998+Kess-Apps@users.noreply.github.com>
2021-01-15 17:38:46 +0100

    Update logedin.js

--- assets/logedin.js
+++ assets/logedin.js
@@ -1,10 +1,10 @@
 function logout(){
 	document.cookie = "login=false";
-	window.open("assets/index.html","_self");
+	window.open("../index.html","_self");
 }
 function deleteAccount(){
 	document.cookie = "username=#";
 	document.cookie = "password=#";
 	document.cookie = "login=false";
-	window.open("assets/index.html","_self");
+	window.open("../index.html","_self");
 }
